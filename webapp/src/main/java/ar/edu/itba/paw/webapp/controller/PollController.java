2d1ff834c690785dce2fcc410b3ba8e06704e866
jsackmann <jsackmann@itba.edu.ar>
2021-04-20 14:21:47 -0700

    merge resolve conflicts

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PollController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PollController.java
@@ -16,6 +16,7 @@ import java.util.*;
 import ar.edu.itba.paw.models.Career;
 import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.Poll;
+import ar.edu.itba.paw.models.HolderEntity;
 import ar.edu.itba.paw.models.Poll.PollOption;
 import ar.edu.itba.paw.models.ui.NavigationItem;
 import ar.edu.itba.paw.services.CareerService;
@@ -23,7 +24,7 @@ import ar.edu.itba.paw.services.CourseService;
 import ar.edu.itba.paw.services.PollService;
 import ar.edu.itba.paw.services.UserService;
 import ar.edu.itba.paw.webapp.mav.BaseMav;
-
+import ar.edu.itba.paw.webapp.form.PollForm;
 import javax.validation.Valid;
 
 @Controller
@@ -37,49 +38,7 @@ public class PollController {
 
     @Autowired private CourseService courseService;
 
-    @RequestMapping("/polls")
-    public ModelAndView getPolls(
-            @RequestParam(name="filterBy", required = false, defaultValue = "general") HolderEntity filterBy,
-            @RequestParam(name = "careerId", required = false) Integer careerId,
-            @RequestParam(name = "courseId", required = false) String courseId,
-            @ModelAttribute("pollForm") final PollForm pollForm
-            ){
-        final ModelAndView modelAndView = new ModelAndView("polls/polls_list");
-        modelAndView.addObject("filterBy", filterBy);
 
-        List<Poll> pollList = new ArrayList<>();
-        List<Career> careers = careerService.findAll();
-
-        modelAndView.addObject("careers", careers);
-
-        List<Course> courses = courseService.findAll();
-        modelAndView.addObject("courses", courses);
-        switch (filterBy){
-            case course:
-                if (courseId != null){
-                    pollList = pollService.findByCourse(courseId);
-                    Course selected = courses.stream().filter(c -> c.getId() == courseId).findFirst()
-                            .orElseThrow(RuntimeException::new);
-                    modelAndView.addObject("selectedCourse", selected);
-                }
-                break;
-            case career:
-                if (careerId != null){
-                    pollList = pollService.findByCareer(careerId);
-                    Career selected = careers.stream().filter(c -> c.getId() == careerId).findFirst()
-                            .orElseThrow(RuntimeException::new);
-                    modelAndView.addObject("selectedCareer", selected);
-                }
-                break;
-            case general:
-            default:
-                pollList = pollService.findGeneral();
-                break;
-        }
-        modelAndView.addObject("polls", pollList);
-        modelAndView.addObject("user", userService.getUser());
-        return modelAndView;
-    }
 
 
     @RequestMapping("/polls")
