5749365eba3e80e925ef99c362b5a692783becfe
lucas <Lucas Gomez>
2021-04-20 17:51:22 -0300

    Error al general html

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PollController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PollController.java
@@ -1,17 +1,19 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.models.HolderEntity;
+import ar.edu.itba.paw.services.UserService;
+import ar.edu.itba.paw.webapp.form.PollForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.DateFormat;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Optional;
+import java.util.*;
 
 import ar.edu.itba.paw.models.Career;
 import ar.edu.itba.paw.models.Course;
@@ -22,14 +24,63 @@ import ar.edu.itba.paw.services.CourseService;
 import ar.edu.itba.paw.services.PollService;
 import ar.edu.itba.paw.webapp.mav.BaseMav;
 
+import javax.validation.Valid;
+
 @Controller
 public class PollController {
+
+    @Autowired private UserService userService;
 
     @Autowired private PollService pollService;
 
     @Autowired private CareerService careerService;
 
     @Autowired private CourseService courseService;
+
+    @RequestMapping("/polls")
+    public ModelAndView getPolls(
+            @RequestParam(name="filterBy", required = false, defaultValue = "general") HolderEntity filterBy,
+            @RequestParam(name = "careerId", required = false) Integer careerId,
+            @RequestParam(name = "courseId", required = false) String courseId,
+            @ModelAttribute("pollForm") final PollForm pollForm
+            ){
+        final ModelAndView modelAndView = new ModelAndView("polls/polls_list");
+        modelAndView.addObject("filterBy", filterBy);
+
+        List<Poll> pollList = new ArrayList<>();
+        List<Career> careers = careerService.findAll();
+
+        modelAndView.addObject("careers", careers);
+
+        List<Course> courses = courseService.findAll();
+        modelAndView.addObject("courses", courses);
+        switch (filterBy){
+            case course:
+                if (courseId != null){
+                    pollList = pollService.findByCourse(courseId);
+                    Course selected = courses.stream().filter(c -> c.getId() == courseId).findFirst()
+                            .orElseThrow(RuntimeException::new);
+                    modelAndView.addObject("selectedCourse", selected);
+                }
+                break;
+            case career:
+                if (careerId != null){
+                    pollList = pollService.findByCareer(careerId);
+                    Career selected = careers.stream().filter(c -> c.getId() == careerId).findFirst()
+                            .orElseThrow(RuntimeException::new);
+                    modelAndView.addObject("selectedCareer", selected);
+                }
+                break;
+            case general:
+            default:
+                pollList = pollService.findGeneral();
+                break;
+        }
+        modelAndView.addObject("polls", pollList);
+        modelAndView.addObject("user", userService.getUser());
+        return modelAndView;
+    }
+
 
     @RequestMapping("polls/byId")
     public ModelAndView getPollDetails(@RequestParam int id)
---
+++ webapp/src/main/webapp/WEB-INF/jsp/polls/poll_card.jsp
@@ -0,0 +1,20 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
+
+<jsp:useBean type="ar.edu.itba.paw.models.Poll" scope="request" id="poll"/>
+
+<div class="card shadow mb-4" style="margin-top: 32px;">
+    <div class="card-header py-3">
+        <h6 class="font-weight-bold m-0"><c:out value="${poll.name}"/></h6>
+    </div>
+    <div class="row">
+        <div class="col mr-2" style="padding: 16px 24px;">
+            <p><c:out value="${poll.description}"/></p>
+        </div>
+        <div class="col mr-10" style=" padding: 16px 24px; text-align: right">
+            <h6><c:out value="Caduca el: ${poll.expiryDate}"/></h6>
+        </div>
+    </div>
+
+</div>
