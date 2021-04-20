ea52ce12d193e78e08623bde4b48a495bd6f6b0e
Joaquín Colonnello <joaquincolonnello@hotmail.com>
2021-04-20 18:06:11 -0300

    Fixed tests

--- persistence/src/test/resources/populators/course_populate.sql
+++ persistence/src/test/resources/populators/course_populate.sql
@@ -11,8 +11,8 @@ INSERT INTO course(id,name) VALUES ('1.2', 'Course 2');
 INSERT INTO career_course(career_id, course_id) VALUES (1, '1.1');
 INSERT INTO career_course(career_id, course_id) VALUES (2, '1.2');
 
-INSERT INTO users(id,name,email,career_id,signup_date) VALUES (1, 'User 1', 'usr1@test.com', 1, default);
-INSERT INTO users(id,name,email,career_id,signup_date) VALUES (2, 'User 2', 'usr2@test.com', 1, default);
+INSERT INTO users(id,name,surname,email,career_id,signup_date) VALUES (1, 'User 1', 'Surname', 'usr1@test.com', 1, default);
+INSERT INTO users(id,name,surname,email,career_id,signup_date) VALUES (2, 'User 2', 'Surname', 'usr2@test.com', 1, default);
 
 INSERT INTO fav_course(course_id, user_id) VALUES ('1.1', 1);
 INSERT INTO fav_course(course_id, user_id) VALUES ('1.2', 2);
