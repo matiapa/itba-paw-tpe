3af36a2c128cf126258c20c9f7ba22fad43f040c
jsackmann <jsackmann@itba.edu.ar>
2021-05-15 18:14:02 -0300

    fixed  string to int error and added digit validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserForm.java
@@ -3,6 +3,7 @@ package ar.edu.itba.paw.webapp.form;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class UserForm {
     private String password;
 
     @NotNull
+    @Digits(integer = 6,fraction = 0)
     private int id;
 
     @NotEmpty
