72d822c2bf0aa53beebef0c93afeb6af0c92ea9c
Matias J. Apablaza <matiglas@hotmail.com>
2021-05-19 10:20:55 -0300

    Beginned implementation of JPA on announcement

--- models/src/main/java/ar/edu/itba/paw/models/Announcement.java
+++ models/src/main/java/ar/edu/itba/paw/models/Announcement.java
@@ -1,28 +1,62 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.*;
+import javax.persistence.Entity;
 import java.util.Date;
 
+@Entity
+@Table(name = "announcement")
 public class Announcement {
 
-    private final int id;
-    private final User uploader;
-    private final String title, summary, content;
-    private final Date uploadDate, expiryDate;
-    private final Boolean seen;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "announcement_id_seq")
+    @SequenceGenerator(sequenceName = "announcement_id_seq", name = "announcement_id_seq", allocationSize = 1)
+    @Column(name = "id")
+    private Integer id;
 
-    public Announcement(int id, User uploader, String name, String summary, String description,
-                        Date uploadDate, Date expiryDate, Boolean seen) {
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "summary", nullable = false)
+    private String summary;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Career career;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Course course;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private User uploader;
+
+    @Column(name = "creation_date", columnDefinition = "date default now() not null")
+    private Date uploadDate;
+
+    @Column(name = "expiry_date")
+    private Date expiryDate;
+
+    private Boolean seen;
+
+    Announcement(){}
+
+    public Announcement(Integer id, String title, String summary, String content, Career career, Course course,
+                        User uploader, Date uploadDate, Date expiryDate, Boolean seen) {
         this.id = id;
         this.uploader = uploader;
-        this.title = name;
+        this.title = title;
         this.summary = summary;
-        this.content = description;
+        this.content = content;
         this.uploadDate = uploadDate;
+        this.career = career;
+        this.course = course;
         this.expiryDate = expiryDate;
         this.seen = seen;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
@@ -52,5 +86,13 @@ public class Announcement {
 
     public Boolean getSeen() {
         return seen;
+    }
+
+    public Career getCareer() {
+        return career;
+    }
+
+    public Course getCourse() {
+        return course;
     }
 }
