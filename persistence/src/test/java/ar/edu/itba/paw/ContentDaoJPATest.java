3e901cca43c730df156d21e7e04359675c33172e
lucas <Lucas Gomez>
2021-06-14 20:09:50 -0300

    Content test changed to jpa

--- persistence/src/test/java/ar/edu/itba/paw/ContentDaoJPATest.java
+++ persistence/src/test/java/ar/edu/itba/paw/ContentDaoJPATest.java
@@ -1,8 +1,10 @@
 package ar.edu.itba.paw;
 
+import ar.edu.itba.paw.models.Career;
 import ar.edu.itba.paw.models.Content;
+import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.User;
-import ar.edu.itba.paw.persistence.ContentDaoJdbc;
+import ar.edu.itba.paw.persistence.jpa.ContentDaoJPA;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,48 +17,63 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 
 @Rollback
 @Sql("classpath:populators/content_populate.sql")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
-public class ContentDaoJdbcTest {
+public class ContentDaoJPATest {
 
-    @Autowired private ContentDaoJdbc contentDao;
-
+    @Autowired private ContentDaoJPA contentDao;
     @Test
-    public void findContentTest() {
-        List<Content> contentList = contentDao.findContent("1.1", Content.ContentType.miscellaneous,null,null,0,1000);
-        Assert.assertEquals(2,contentList.size());
-        Assert.assertEquals(1,contentList.get(0).getId());
-        Assert.assertEquals(2,contentList.get(1).getId());
-    }
+    public void findByCourseTest() {
+        Course course = new Course();
+        course.setId("1.1");
 
+        List<Content> contentList = contentDao.findByCourse(course, Content.ContentType.miscellaneous,null,null,0,10);
+        Assert.assertEquals(2,contentList.size());
+        Assert.assertEquals(1, Optional.ofNullable(contentList.get(0).getId()));
+        Assert.assertEquals(2, Optional.ofNullable(contentList.get(1).getId()));
+    }
     @Test
     public void deleteTest(){
-        List<Content> contentList = contentDao.findContent("1.1", Content.ContentType.miscellaneous,null,null,0,10);
+        Course course = new Course();
+        course.setId("1.1");
+
+        List<Content> contentList = contentDao.findByCourse(course, Content.ContentType.miscellaneous,null,null,0,10);
         Assert.assertEquals(2,contentList.size());
-        Assert.assertEquals(1,contentList.get(0).getId());
-        Assert.assertEquals(2,contentList.get(1).getId());
+        Assert.assertEquals(1, Optional.ofNullable(contentList.get(0).getId()));
+        Assert.assertEquals(2, Optional.ofNullable(contentList.get(1).getId()));
 
-        contentDao.delete(1);
+        contentDao.delete(contentList.get(0));
 
-        contentList = contentDao.findContent("1.1",Content.ContentType.miscellaneous,null,null,0,10);
+        contentList = contentDao.findByCourse(course,Content.ContentType.miscellaneous,null,null,0,10);
         Assert.assertEquals(1,contentList.size());
-        Assert.assertEquals(2,contentList.get(0).getId());
+        Assert.assertEquals(2, Optional.ofNullable(contentList.get(0).getId()));
     }
 
     @Test
     public void createContentTest(){
+        Career career = new Career();
+        career.setCode("S");
+        Course course = new Course();
+        course.setId("1.1");
+        User user = new User(0, "John", "Doe", "jd@gmail.com", "12345678", career);
 
-        List<Content> contentList = contentDao.findContent("1.1",Content.ContentType.miscellaneous,null,null,0,10);
+        List<Content> contentList = contentDao.findByCourse(course,Content.ContentType.miscellaneous,null,null,0,10);
         Assert.assertEquals(2,contentList.size());
+/*
+*     public Content createContent(String name, String link, Course course, String description, ContentType contentType,Date contentDate, User uploader, String ownerMail) {
+ */
+        Content content = new Content();
+        content.setName("name");
+        content.setLink("www.test.com");
+        Assert.assertEquals(content ,contentDao.createContent("name", "www.test.com", course, "test description", Content.ContentType.miscellaneous, null,
+                user, user.getEmail()));
 
-        Assert.assertTrue(contentDao.createContent("name", "www.test.com", "1.1", "test description", "miscellaneous", null,
-                new User(1, "test", "test surname", "testEmail@gmail.com", "password", null, new Date(), null, "S")));
-
-        contentList = contentDao.findContent("1.1", Content.ContentType.miscellaneous,null,null,0,10);
+        contentList = contentDao.findByCourse(course, Content.ContentType.miscellaneous,null,null,0,10);
         Assert.assertEquals(3,contentList.size());
 
     }
