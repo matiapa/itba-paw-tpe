2be4bfa21fea12198a762888d874d79de9a1c586
lucas <Lucas Gomez>
2021-06-16 00:40:03 -0300

    Error in tests

--- persistence/src/test/java/ar/edu/itba/paw/TestConfig.java
+++ persistence/src/test/java/ar/edu/itba/paw/TestConfig.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw;
 
+import org.hsqldb.Session;
 import org.hsqldb.jdbc.JDBCDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +8,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @ComponentScan({ "ar.edu.itba.paw.persistence", })
 @Configuration
@@ -24,9 +34,11 @@ public class TestConfig {
 
     @Bean
     public DataSource dataSource() {
-        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        final SingleConnectionDataSource ds = new SingleConnectionDataSource();
+        //final SimpleDriverDataSource ds = new SimpleDriverDataSource();
 
-        ds.setDriverClass(JDBCDriver.class);
+       // ds.setDriverClass(JDBCDriver.class);
+        ds.setSuppressClose(true);
         ds.setUrl("jdbc:hsqldb:mem:paw");
         ds.setUsername("ha");
         ds.setPassword("");
@@ -45,6 +57,7 @@ public class TestConfig {
     }
 
     private DatabasePopulator databasePopulator(){
+
         final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
 
         dbp.addScript(hsqldbSql);
@@ -52,5 +65,35 @@ public class TestConfig {
 
         return dbp;
     }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        final LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setPackagesToScan("ar.edu.itba.paw.persistence");
+        factoryBean.setDataSource(dataSource());
+
+        final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+
+        final Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.ddl.auto", "none");
+
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+/*
+        // TODO: Remove this for production
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("format_sql", "true");
+*/
+        factoryBean.setJpaProperties(properties);
+
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
 
 }
